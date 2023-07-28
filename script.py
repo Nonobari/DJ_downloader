a0f0fa127a0987c1a13e3aae492cd00408925e50
Nonobari <noe.backert@gmail.com>
2023-07-28 19:43:47 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,48 @@
+import sys
+from pytube import YouTube, Playlist
+from pytube.cli import on_progress
+from pytube.helpers import safe_filename
+from tqdm import tqdm
+from moviepy.editor import *
+
+def get_highest_audio(url, path = "music"):
+    video = YouTube(url, on_progress_callback=on_progress)
+    # Download the highest audio resolution save it in mp3 format and make a progress bar
+    print("downloading....")
+    file_name = f"{safe_filename(f'{video.title} - {video.author}')}.wav"
+    video.streams.filter(only_audio = True).order_by('abr').desc().first().download(output_path= path, filename=file_name)
+    print("Downloaded! :)")
+
+def download_playlist(url, path = "music"):
+    playlist = Playlist(url)
+    n = len(playlist.videos)
+    print(f"Number of videos in playlist: {n}")
+    for video in tqdm(playlist.videos):
+        try :
+            # remove impossible characters from the title
+            file_name = f"{safe_filename(f'{video.title} - {video.author}')}.wav"
+            video.streams.filter(only_audio = True).order_by('abr').desc().first().download(output_path= path, filename=file_name)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            sys.exit(1)
+        except:
+            print(f"Error downloading {file_name}")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide a link")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        if "playlist" in sys.argv[1]:
+            download_playlist(sys.argv[1])
+        elif "watch" in sys.argv[1]:
+            get_highest_audio(sys.argv[1])
+    elif len(sys.argv) == 3:
+        if "playlist" in sys.argv[1]:
+            download_playlist(sys.argv[1], sys.argv[2])
+        elif "watch" in sys.argv[1]:
+            get_highest_audio(sys.argv[1], sys.argv[2])
+
+
+if __name__ == "__main__":
+    main()
